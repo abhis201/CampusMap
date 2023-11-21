1d6f89d5ad1d953b8a1610173a8d6a224347a25d
Daniel Smith <smit3188@pnw.edu>
2023-11-20 23:33:23 -0600

    Implemented use of environment variables - DSmith

--- client/src/components/CampusMap.jsx
+++ client/src/components/CampusMap.jsx
@@ -3,19 +3,22 @@ import { GoogleMap, LoadScript, MarkerF } from '@react-google-maps/api';
 
 const CampusMap = ({ markers }) => {
 
-   // Set your Google Maps API key here
-  const apiKey = 'AIzaSyDcwsBTuzqUTjhN03jD2_LqCqH0nDYCZ6U';
+  // Set your Google Maps API key here
+  const apiKey = import.meta.env.VITE_GMAP_API_KEY;
 
   // Coordinates for your campus boundaries
   const campusBounds = {
-    north: 31.5865,
-    south: 51.584,
-    west: -97.476,
-    east: -77.470,
+    north: Number(import.meta.env.VITE_NORTH),
+    south: Number(import.meta.env.VITE_SOUTH),
+    west: Number(import.meta.env.VITE_WEST),
+    east: Number(import.meta.env.VITE_EAST),
   };
 
   // Center coordinates for your map
-  const campusCoordinates = { lat: 41.585118752281346, lng: -87.47408630819197 };
+  const campusCoordinates = { 
+    lat: Number(import.meta.env.VITE_CENTER_LAT), 
+    lng: Number(import.meta.env.VITE_CENTER_LNG)
+  };
 
   return (
     <LoadScript id='mapComp' googleMapsApiKey={apiKey}>
